72d96f80d616c883fac8f3cd02b091bcfc8fbf00
dnomjr <dano.majer@protonmail.com>
2024-08-07 21:15:27 +0200

    header edit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,14 @@
 'use strict'
 
-const openBtn = document.querySelector('.open')
-const closeBtn = document.querySelector('.close')
-const menu = document.querySelector('.overlay-nav')
+const openBtn = document.querySelector('.menu-btn')
+/* const closeBtn = document.querySelector('.close')
+ */ const menu = document.querySelector('.overlay-nav')
 
 const toggleNav = function () {
   menu.classList.toggle('show')
+  openBtn.classList.toggle('open')
 }
 
 openBtn.addEventListener('click', toggleNav)
-closeBtn.addEventListener('click', toggleNav)
+/* closeBtn.addEventListener('click', toggleNav)
+ */
