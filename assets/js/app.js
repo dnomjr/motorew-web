ccb23da6de91b6eeab963eac3677e0480f185f06
dnomjr <dano.majer@protonmail.com>
2024-08-19 07:40:31 +0200

    responsive update

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,19 +3,55 @@
 const menuIcon = document.querySelector('.menu-btn')
 const menuOverlay = document.querySelector('.overlay')
 const links = document.querySelectorAll('nav a')
+const slides = document.querySelectorAll('.slide')
+const leftArrow = document.querySelector('.left-arrow')
+const rightArrow = document.querySelector('.right-arrow')
+
+let currSlide = 0
+const maxSlide = slides.length
 
 const showMenu = () => {
   menuOverlay.classList.toggle('show')
   menuIcon.classList.toggle('open')
 }
 
-const scrollToSection = function (e) {
-  /*   e.preventDefault()
-   */ showMenu()
-}
-
 links.forEach(link => {
-  link.addEventListener('click', scrollToSection)
+  link.addEventListener('click', showMenu)
 })
 
+const goToSlide = function (slide) {
+  slides.forEach((s, i) => {
+    s.style.transform = `translateX(${100 * (i - slide)}%)`
+  })
+}
+const nextSlide = function () {
+  if (currSlide === maxSlide - 1) {
+    currSlide = 0
+  } else {
+    currSlide++
+  }
+  goToSlide(currSlide)
+}
+
+const prevSlide = function () {
+  if (currSlide === 0) {
+    currSlide = maxSlide - 1
+  } else {
+    currSlide--
+  }
+
+  goToSlide(currSlide)
+}
+
+const init = function () {
+  goToSlide(currSlide)
+}
+init()
+
 menuIcon.addEventListener('click', showMenu)
+rightArrow.addEventListener('click', nextSlide)
+leftArrow.addEventListener('click', prevSlide)
+
+/* const scrollToSection = function (e) {
+  showMenu()
+} */
---
+++ assets/js/data.js
@@ -0,0 +1,24 @@
+const reviews = [
+  {
+    name: 'Norbert Bomba',
+    text: 'Vysoko profesionálny prístup, najlepší šéf servisu na východnom Slovensku a priľahlom vesmíre. Ak im zveríte svoju lásku, postarajú sa o ňu ako o vlastnú.',
+    source: 'google',
+  },
+  {
+    name: 'Peter Horváth',
+    text: 'Potreboval som rýchlu opravu, aby som sa dostal na dôležitú cestu. V tomto servise mi auto opravili za rekordný čas a to bez kompromisov na kvalite. Mechanici boli profesionáli a ochotne mi vysvetlili, čo bolo potrebné urobiť.',
+    source: 'google',
+  },
+  {
+    name: 'Pavol Marek',
+    text: 'Ceny boli férové a celkovo som mal pocit, že sa o moje auto dobre postarali.',
+    source: 'google',
+  },
+  {
+    name: 'Jozef Horváth',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?',
+    source: 'google',
+  },
+]
+
+export default reviews
