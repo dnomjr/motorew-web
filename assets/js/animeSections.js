e047402f52cf93d78b72432bfe46544c2b7fecee
dnomjr <dano.majer@protonmail.com>
2024-08-20 11:39:13 +0200

    js update

--- assets/js/animeSections.js
+++ assets/js/animeSections.js
@@ -2,15 +2,30 @@ const sections = document.querySelectorAll('section')
 
 const revealSection = function (entries, observer) {
   const [entry] = entries
+  console.log(
+    entry.isIntersecting, entry.intersectionRatio,
+    entry.target.classList.contains('about')
+  )
 
   if (!entry.isIntersecting) return
+  if (entry.isIntersecting &&
+    entry.intersectionRatio > 0.15 &&
+    entry.target.classList.contains('about')
+  ) {
+    setTimeout(() => {
+      entry.target.classList.remove('section-hidden')
+    },1000)
+    return
+  }
 
   entry.target.classList.remove('section-hidden')
   observer.unobserve(entry.target)
 }
 const sectionObserver = new IntersectionObserver(revealSection, {
   root: null,
-  threshold: 0.10,
+  threshold: 0.1,
+  /*   rootMargin: '-90px',
+   */
 })
 
 sections.forEach(function (section) {
