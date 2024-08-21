0ad0e1bbd15bf2a33f29c60306fc2f7729500f00
dnomjr <dano.majer@protonmail.com>
2024-08-21 16:31:06 +0200

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,14 +12,46 @@ const leftArrow = document.querySelector('.left-arrow')
 const sliderOutline = document.querySelector('.slider-nav')
 const slidesContainer = document.querySelector('.slider')
 const actualYear = document.querySelector('.year')
-
-const date = new Date().getFullYear()
-actualYear.textContent = date
-
-const swipePrevSlide = swipeNextSlide.bind('prev')
+const motorbike = document.querySelector('.after')
 
 let startingX, endingX
 let moving = false
+const endPosition = 50
+
+const swipePrevSlide = swipeNextSlide.bind('prev')
+const date = new Date().getFullYear()
+
+let lastScroll = 0
+
+let startPosition = parseInt(getComputedStyle(motorbike).backgroundPosition)
+
+const moveOnScroll = function (e) {
+  let scrollPosition = Math.floor(window.scrollY) / 2
+  console.log(scrollPosition, startPosition)
+
+  if (scrollPosition > lastScroll) {
+  }
+
+  if (startPosition - scrollPosition <= endPosition) return
+
+  motorbike.style.backgroundPosition = startPosition - scrollPosition + '%'
+
+  /*if (currentScroll > lastScroll) {
+    for (let i = lastScroll + 1; i <= currentScroll; i++) {
+      if (i < 1000)
+        motorbike.style.backgroundPosition =
+          startPosition + currentScroll + 'px'
+    }
+  }  else if (currentScroll < lastScroll) {
+    for (let i = lastScroll - 1; i >= currentScroll; i--) {
+      console.log(i)
+      }
+      } 
+
+  lastScroll = currentScroll*/
+}
+
+actualYear.textContent = date
 
 const init = function () {
   createSlides()
@@ -53,3 +85,5 @@ sliderOutline.addEventListener('click', switchSlide)
 slidesContainer.addEventListener('touchstart', start)
 slidesContainer.addEventListener('touchmove', move)
 slidesContainer.addEventListener('touchend', end)
+
+window.addEventListener('scroll', moveOnScroll)
