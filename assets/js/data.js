89a1d96d64a68b3ea70f244cbef161253e243b75
dnomjr <dano.majer@protonmail.com>
2024-08-30 13:13:36 +0200

    last updates

--- assets/js/data.js
+++ assets/js/data.js
@@ -6,17 +6,17 @@ const reviews = [
   },
   {
     name: 'Ján M.',
-    text: 'Veľmi šikovný servis. Vedia dobre a ochotne poradiť a opravia všetky typy motocyklov. Odporúčam. Parkovanie bezproblémové. Šikovný, palec hore',
+    text: 'Veľmi šikovný servis. Vedia dobre a ochotne poradiť a opravia všetky typy motocyklov. Odporúčam. Parkovanie bezproblémové. Šikovný, palec hore.',
     source: 'google',
   },
   {
     name: 'Miroslav U.',
-    text: 'Keď spoja najlepší s najlepšími výsledok je vždy spokojnosť. Ďakujem za servis mojej cbr1000f a gt650',
+    text: 'Keď sa spoja najlepší s najlepšími výsledok je vždy spokojnosť. Ďakujem za servis mojej cbr1000f a gt650.',
     source: 'google',
   },
   {
     name: 'Tomáš R.',
-    text: 'Úžasný servis, dobrý mechanik, rýchle dodanie dielov.  Diagnostika trvala 4 minúty a diely prišli na druhý deň. Za menej ako 24 hodín som opäť šoféroval ako obvykle. Úžasné!',
+    text: 'Úžasný servis, dobrý mechanik, rýchle dodanie dielov. Diagnostika trvala 4 minúty a diely prišli na druhý deň. Za menej ako 24 hodín som opäť šoféroval ako obvykle. Úžasné!',
     source: 'google',
   },
 ]
